13d8b00f63857996496b5b2133aebc13c5159e9e
RealBokito <42062587+RealBokito@users.noreply.github.com>
2019-09-23 14:21:11 +0200

    Update and rename RIP0014/ReadValidateTest.php to Proof of Concept/ReadValidateTest.php

--- Proof of Concept/ReadValidateTest.php
+++ Proof of Concept/ReadValidateTest.php
@@ -47,13 +47,14 @@ $data = json_decode($json_data,true);
 foreach($data['metadata'] as $obj){
 	//echo "Using schema: ".$obj['schema']." ";
 	//echo "version: ".$obj['version']."\r\n";
-	
+	echo "Checking ".$obj['schema']." meta data.....\r\n";
 	//  Validate the Meta Data
 	$ret = validateMetaData($obj);
 	if($ret){
-		var_dump($obj);
+		echo $obj['schema']." meta data is valid\r\n\r\n";
+		//var_dump($obj);
 	}else{
-		echo $obj['schema']." meta data invalid\r\n";
+		echo $obj['schema']." meta data invalid\r\n\r\n";
 	}
 }
 
@@ -70,33 +71,67 @@ function validateMetaData($data){
 		$schema = json_decode($schema,true);
 		
 		//  Get the fieldnames from the Schema by abstracting the array keys
-		$schemaFields = array_keys($schema);
-		
-		//  Loop through all the fields
-		foreach($schemaFields as $field){
-			if($field == "schema" || $field == "version")//  Exlude the schema and version fields
-				continue;
-			
-			//  Check if a field is SET
-			if(isset($data[$field])){
-				//echo $field." is SET\r\n";
-			}else{
-				//  Check if the NOT SET field is required
-				if($schema[$field]['required']){
-					//  Invalidate the Meta Data because at least 1 field is missing
-					echo "Schema validation error: ".$field."-field is required...\r\n";
-					return false;
-				}
-				//  The current field is missing, but not required
-				echo $field." is NOT set\r\n";
-			}
-		}
+		//$schemaFields = array_keys($schema);
+		//var_dump($data);
+		//var_dump($schema);
+		$ret = validate($data,$schema);
+		if(!$ret)
+			return false;
 	}else{
 		//  The software engineer will have to decide how it's program will behave when there is nog Schema file found
-		//echo "Schema file not found.\r\n";
+		echo "Schema file not found.\r\n";
+		echo "Assume ";
 	}
 	
 	//  All is good. Meta Data is valid
+	return true;
+}
+
+function validate($data,$schema,$level=1){
+	echo "Level: ".$level."...\r\n";
+	//var_dump($data);
+	//var_dump($schema);
+	//  Get the fieldnames from the Schema by abstracting the array keys
+	$schemaFields = array_keys($schema);
+	
+	//  Loop through all the fields
+	foreach($schemaFields as $schemaField){
+		if($schemaField == "schema" || $schemaField == "version")//  Exlude the schema and version fields
+			continue;
+		
+		//  Check if a field is SET
+		if(isset($data[$schemaField]) && !is_array($data[$schemaField])){
+			echo $schemaField." is SET\r\n";
+		}elseif(isset($data[$schemaField]) && is_array($data[$schemaField])){
+			$level++;
+			$ret = validate($data[$schemaField],$schema[$schemaField]['fields'],$level);			
+			if(!$ret)
+				return false;
+			$level--;
+			echo "Back at level: ".$level."\r\n";
+		}else{
+			//  Check if the NOT SET field is required
+			if($schema[$schemaField]['required']){
+				//  Invalidate the Meta Data because at least 1 field is missing
+				echo "Schema validation error: ".$schemaField."-field is required...\r\n";
+				return false;
+			}
+			//  The current field is missing, but not required
+			echo $schemaField." is NOT set\r\n";
+		}
+	}
+	
+	//  Do one final check for fields described in the metadata but NOT in the schema and mark them as Unknown Field(s)
+	$dataFields = array_keys($data); //  Get keys from the metadata
+	$unknownFields = array_diff($dataFields, $schemaFields); //  Calculate the differences between the metadata keys and schema keys
+	//  If any metadata keys are not found in the schema keys array, mark as unknown...
+	if($unknownFields){
+		foreach($unknownFields as $unknownField){
+			echo $unknownField."  is an unknown field.\r\n";
+		}
+	}
+	
+	//  If we get here, assume everything is OK
 	return true;
 }
 ?>
